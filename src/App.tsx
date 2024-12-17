47544ede1286a46d13767521a4bc36092b803ba8
lVenhoff <Louis.venhoff@pfreundt.de>
2024-12-17 15:32:54 +0100

    Refresh assets on every hour

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import AssetView from './components/assetView/assetView'
-import AssetLoader from './libs/assetLoader';
-import Asset from './classes/asset';
+import { useEffect, useState } from "react";
+import "./App.css";
+import AssetView from "./components/assetView/assetView";
+import AssetLoader from "./libs/assetLoader";
+import Asset from "./classes/asset";
 
 function App() {
-
   const [assets, setAssets] = useState<Asset[]>([]);
 
   useEffect(() => {
     loadAssets();
+    setInterval(() => {
+      loadAssets();
+    }, 3600000)
   }, []);
 
-
   const loadAssets = async () => {
-    const assets: Asset[] = await AssetLoader.loadAssets();
+      const assets: Asset[] = await AssetLoader.loadAssets();
 
-    setAssets(assets);
-  }
+      setAssets(assets);
+  };
 
-  return (
-    <>
-      {assets.length > 0 ? <AssetView assets={assets} /> : <h1>Loading Assets</h1>}
-    </>
-  )
+  return <>{assets.length > 0 ? <AssetView assets={assets} /> : <h1>Loading Assets</h1>}</>;
 }
 
-export default App
+export default App;
--- src/components/assetView/assetView.tsx
+++ src/components/assetView/assetView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./assetViewStyle.css";
 import Asset from "../../classes/asset";
 import ImageAsset from "../../classes/imageAsset";
@@ -13,6 +13,7 @@ const AssetView: React.FC<AssetViewProps> = ({assets}) => {
 
   const [assetList, setAssetList] = useState<Asset[]>([]);
   const [displayElement, setDisplayElement] = useState<JSX.Element | null>(null);
+  const interval = useRef(null);
 
 
   useEffect(() => {
@@ -21,13 +22,14 @@ const AssetView: React.FC<AssetViewProps> = ({assets}) => {
   }, [assets]);
 
   useEffect(() => {
+    clearInterval(interval.current);
     startDiashow();
   },[assetList]);
 
   const startDiashow = () => {
 
     let counter: number = 0;
-    setInterval(() => {
+    interval.current = setInterval(() => {
       if (assetList[counter] instanceof ImageAsset) {
         const current: ImageAsset = assetList[counter] as ImageAsset;
         setDisplayElement(renderDisplay(false, `data:image/png;base64,${current.data}`));
